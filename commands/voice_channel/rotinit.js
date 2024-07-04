a7be373deb49e2fdd3a1ae8ade74eb8e3f13ac48
Michal <michalzajdel2002@gmail.com>
2024-07-04 20:33:29 +0200

    improved rotinit

--- commands/voice_channel/rotinit.js
+++ commands/voice_channel/rotinit.js
@@ -7,27 +7,51 @@ const {
   NoSubscriberBehavior,
 } = require("@discordjs/voice");
 
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("rotinit")
-    .setDescription("Initiate the rot"),
+    .setDescription("Initiate the rot")
+    .addStringOption((option) =>
+      option
+        .setName("rot_option")
+        .setDescription("Option for rotting")
+        .setRequired(false)
+    ),
   async execute(interaction) {
+    const directoryPath = path.join(__dirname, "../..", "audio");
+    let files;
+    try {
+      files = fs.readdirSync(directoryPath);
+    } catch (err) {
+      console.error(`Error reading directory: ${err}`);
+      process.exit(1);
+    }
+    const mp3Files = files
+      .filter((file) => path.extname(file) === ".mp3")
+      .map((file) => path.basename(file, ".mp3"));
+
+    var option = interaction.options.getString("rot_option");
     const connection = getVoiceConnection(interaction.guild.id);
     if (!connection) {
       return await interaction.reply(
         "Nie jestem na kanale, więc nie mogę gnić!"
       );
     }
+    if (!mp3Files.includes(option)) {
+      const randomIndex = Math.floor(Math.random() * mp3Files.length);
+      option = mp3Files[randomIndex];
+    }
     const audioPlayer = createAudioPlayer({
       behaviors: {
         noSubscriber: NoSubscriberBehavior.Pause,
       },
     });
-    const resource = createAudioResource(
-      "C:/Users/micha/Desktop/RottenBot/audio/skibidipl.mp3"
-    );
-    audioPlayer.play(resource);
+    const resource = createAudioResource(directoryPath + "/" + option + ".mp3");
     connection.subscribe(audioPlayer);
+    audioPlayer.play(resource);
     audioPlayer.on(AudioPlayerStatus.Playing, () => {
       return interaction.reply("Enjoy!🙂");
     });
