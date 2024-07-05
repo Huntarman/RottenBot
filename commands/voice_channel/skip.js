61de8cec08e6d5aabdc0c9e610bef02f2e223298
Michal <michalzajdel2002@gmail.com>
2024-07-05 18:45:57 +0200

    expanded skip options

--- commands/voice_channel/skip.js
+++ commands/voice_channel/skip.js
@@ -5,9 +5,16 @@ const { AudioPlayer } = require("../util/AudioPlayer");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("skip")
-    .setDescription("Skip the current song"),
+    .setDescription("Skip the current song")
+    .addStringOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("How many songs to skip?")
+        .setRequired(false)
+    ),
   async execute(interaction) {
     const serverQueue = queue.get(interaction.guild.id);
+    console.log(serverQueue);
     if (
       interaction.member.voice.channelId !==
       serverQueue.connection.joinConfig.channelId
@@ -19,6 +26,30 @@ module.exports = {
 
     if (!serverQueue) {
       return await interaction.reply("Nie ma nic do skipowania!");
+    }
+    const amount = interaction.options.getString("amount");
+    console.log(amount);
+    console.log(serverQueue.songs.length);
+    if (amount) {
+      if (
+        amount.toLowerCase() === "all" ||
+        amount >= serverQueue.songs.length
+      ) {
+        serverQueue.connection.destroy();
+        queue.delete(interaction.guild.id);
+        return await interaction.reply("Skipuje wszystko");
+      }
+      if (amount < 1) {
+        return await interaction.reply("Podaj liczbę większą od 0");
+      } else {
+        for (let i = 0; i < amount; i++) {
+          serverQueue.songs.shift();
+        }
+        AudioPlayer(interaction.guild, serverQueue.songs[0]);
+        return await interaction.reply(
+          `Skipuje piosenki - ilosc: **${amount}**`
+        );
+      }
     }
     if (serverQueue.songs.length === 1) {
       serverQueue.connection.destroy();
