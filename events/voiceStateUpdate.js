1a736bfb1c86703ce1afdf7d6f32ee045a08d4d1
Michal <michalzajdel2002@gmail.com>
2024-07-08 18:57:16 +0200

    clearer logs for channel join/leave

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -4,9 +4,13 @@ module.exports = {
   name: Events.VoiceStateUpdate,
   async execute(oldState, newState) {
     if (newState.channelId === null) {
-      console.log(newState.member.user.tag + " has left the channel");
+      console.log(
+        newState.member.user.tag + ` has left ${oldState.channel.name}`
+      );
     } else if (oldState.channelId === null) {
-      console.log(newState.member.user.tag + " has joined the channel");
+      console.log(
+        newState.member.user.tag + ` has joined ${newState.channel.name}`
+      );
     }
   },
 };
