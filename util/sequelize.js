58034c48e096b61cae5abcb3ecec3718ffb02798
Michal <michalzajdel2002@gmail.com>
2024-08-22 14:40:41 +0200

    ORM connection to database

---
+++ util/sequelize.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+// Initialize Sequelize
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: "localhost",
+    port: 5433,
+    dialect: "postgres",
+  }
+);
+
+module.exports = sequelize;
