51f35490fc0b45cb19f5f882c75a9998dc01e841
Michal <michalzajdel2002@gmail.com>
2024-08-27 14:02:36 +0200

    expanded duel

--- commands/gamblecore/duel.js
+++ commands/gamblecore/duel.js
@@ -30,6 +30,43 @@ module.exports = {
     if (challenger.id === target.id) {
       return interaction.reply("You can't duel yourself!");
     }
+    if (target.bot) {
+      return interaction.reply("You can't duel a bot!");
+    }
+    if (amount < 1) {
+      return interaction.reply("You must duel for at least 1 token!");
+    }
+    try {
+      const challengerProfile = await GuildUserProfile.findOne({
+        where: { user_id: challenger.id, guild_id: interaction.guild.id },
+      });
+      const targetProfile = await GuildUserProfile.findOne({
+        where: { user_id: target.id, guild_id: interaction.guild.id },
+      });
+      if (!challengerProfile) {
+        return interaction.reply(
+          "You do not have a gambler profile yet! Use /gamblestatus to create one."
+        );
+      }
+      if (!targetProfile) {
+        return interaction.reply(
+          "You can't duel someone who doesn't have a gambler profile!"
+        );
+      }
+      if (challengerProfile.tokens < amount) {
+        return interaction.reply("You don't have enough tokens to duel!");
+      }
+      if (targetProfile.tokens < amount) {
+        return interaction.reply(
+          "Your target doesn't have enough tokens to duel!"
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      return interaction.reply(
+        "There was an error while executing this command!"
+      );
+    }
     await interaction.reply("A brawl is surely brewing!");
     const embed = new EmbedBuilder()
       .setTitle("Duel Challenge!")
@@ -155,31 +192,86 @@ const Duel = async (interaction, challenger, target, amount) => {
 
     collector.on("end", async () => {
       if (controller) return;
-      if (choicesMap[interaction.user.id] && choicesMap[target.id]) {
-        const player1Choice = choicesMap[interaction.user.id];
-        const player2Choice = choicesMap[target.id];
-        controller = true;
-        let result = "";
-        if (player1Choice === player2Choice) {
-          result = `It's a tie! You both chose ${player1Choice}.`;
-        } else if (
-          (player1Choice === "rock" && player2Choice === "scissors") ||
-          (player1Choice === "paper" && player2Choice === "rock") ||
-          (player1Choice === "scissors" && player2Choice === "paper")
-        ) {
-          result = `${interaction.user.username} wins! ${player1Choice} beats ${player2Choice}.`;
-        } else {
-          result = `${target.username} wins! ${player2Choice} beats ${player1Choice}.`;
-        }
+      controller = true;
+      try {
+        const challengerProfile = await GuildUserProfile.findOne({
+          where: { user_id: challenger.id, guild_id: interaction.guild.id },
+        });
+        const targetProfile = await GuildUserProfile.findOne({
+          where: { user_id: target.id, guild_id: interaction.guild.id },
+        });
+        if (choicesMap[interaction.user.id] && choicesMap[target.id]) {
+          const player1Choice = choicesMap[interaction.user.id];
+          const player2Choice = choicesMap[target.id];
 
-        await interaction.user.send(result);
-        await target.send(result);
-      } else {
-        await interaction.user.send(
-          "The duel timed out because one or both players did not make a choice."
-        );
-        await target.send(
-          "The duel timed out because one or both players did not make a choice."
+          let result = "";
+          if (player1Choice === player2Choice) {
+            result = `It's a tie! You both chose ${player1Choice}.`;
+          } else if (
+            (player1Choice === "rock" && player2Choice === "scissors") ||
+            (player1Choice === "paper" && player2Choice === "rock") ||
+            (player1Choice === "scissors" && player2Choice === "paper")
+          ) {
+            result = `${interaction.user.username} wins! ${player1Choice} beats ${player2Choice}.\n<@${interaction.user.id}> won ${amount} tokens.\n<@${target.id}> lost ${amount} tokens.`;
+            challengerProfile.tokens += amount;
+            targetProfile.tokens -= amount;
+            await challengerProfile.save();
+            await targetProfile.save();
+          } else {
+            result = `<@${target.id}> wins! ${player2Choice} beats ${player1Choice}.\n<@${interaction.user.id}> lost ${amount} tokens.\n<@${target.id}> won ${amount} tokens.`;
+            challengerProfile.tokens -= amount;
+            targetProfile.tokens += amount;
+            await challengerProfile.save();
+            await targetProfile.save();
+          }
+          await interaction.channel.send(result);
+        } else {
+          const player1Choice = choicesMap[interaction.user.id];
+          const player2Choice = choicesMap[target.id];
+          if (!player1Choice && !player2Choice) {
+            await interaction.channel.send(
+              "Both players have not made their choice. The duel has been cancelled. No tokens have been lost or won."
+            );
+            duelMsgChallenger.delete();
+            duelMsgTarget.delete();
+          } else if (!player1Choice && player2Choice) {
+            const updatedEmbed = EmbedBuilder.from(
+              duelMsgChallenger.embeds[0]
+            ).setDescription("You chose nothing and lost shamefully.");
+            await duelMsgChallenger.edit({
+              content: `You chose nothing. The duel has ended.`,
+              embeds: [updatedEmbed],
+              components: [],
+            });
+            await interaction.channel.send(
+              `<@${interaction.user.id}> did not make a choice. <@${target.id}> wins by default.\n<@${interaction.user.id}> lost ${amount} tokens.\n<@${target.id}> won ${amount} tokens.`
+            );
+            challengerProfile.tokens -= amount;
+            targetProfile.tokens += amount;
+            await challengerProfile.save();
+            await targetProfile.save();
+          } else if (player1Choice && !player2Choice) {
+            const updatedEmbed = EmbedBuilder.from(
+              duelMsgTarget.embeds[0]
+            ).setDescription("You chose nothing and lost shamefully.");
+            await duelMsgTarget.edit({
+              content: `You chose nothing. The duel has ended.`,
+              embeds: [updatedEmbed],
+              components: [],
+            });
+            await interaction.channel.send(
+              `<@${target.id}> did not make a choice. <@${interaction.user.id}> wins by default.\n<@${interaction.user.id}> won ${amount} tokens.\n<@${target.id}> lost ${amount} tokens.`
+            );
+            challengerProfile.tokens += amount;
+            targetProfile.tokens -= amount;
+            await challengerProfile.save();
+            await targetProfile.save();
+          }
+        }
+      } catch (error) {
+        console.error(error);
+        return interaction.reply(
+          "There was an error while executing this command!"
         );
       }
     });
